03c6707b915be5f6e0a991450c76af2563c18d30
Jrkeeling23 <jrkeeling23@gmail.com>
2019-02-13 19:41:54 -0700

    added basic functions

---
+++ csci-305-basic-lisp/square.lisp
@@ -0,0 +1,3 @@
+(defun sqr(x) (* x x) (write x) (print (* x x)))
+(setq x 5)
+(sqr x)
---
+++ csci-305-basic-lisp/summaton-list.lisp
@@ -0,0 +1,32 @@
+(defvar v 5)
+(print v)
+(defun sumNumbers (list)
+    (cond 
+        ((null list)
+            0)
+        ((numberp (car list)) ; checks if it is a number
+            (+ (car list) (sumNumbers (cdr list))))
+        ((symbolp (car list)) ; symbol predicate. if symbol,returns true
+            (sumNumbers (cdr list)))
+        (t ; testing if it is a list, or use t as an else
+        (+ (sumNumbers (car list)) (sumNumbers (cdr list))))))
+
+(setq numberList '(1 2 3 )) ; initialize variable for list. list denoted as '
+(print numberList) ; print initial list
+(sumNumbers numberList) ; calls function to sum numbers
+
+(defun m-length (list) (print list)
+    (if (null list)
+        0
+        (+ 1 (m-length (cdr list)))))
+
+(setq numberList '(1 2 3 )) ; initialize list
+(m-length numberList) ; calls function to get length
+;; or
+;; (defun m-length (list)
+;; (length list))
+
+(defun print-list (list)
+    (if (not (null list))
+        (progn
+        (printc (car list)))))
