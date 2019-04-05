d9547a7a97050c9da3931665addd447731bdf1c9
Jrkeeling23 <jrkeeling23@gmail.com>
2019-04-05 12:28:54 -0600

    Almodt finished

---
+++ csci-305-assignment5-krotlin/kotlinExamplesFromClass/DataTypes.kt
@@ -0,0 +1,19 @@
+
+
+fun main() {
+ 
+    //imutable and immatuble
+    //unchangable
+    val name = "Hunter"    //immutable - read only variables
+    var secondName = "Lloyd" //mutable
+    //println(name + "     " + secondName)  //Got rid of System.out....thank you
+    //type inference
+    //erorrs not being used
+    var bigInt: Int= Int.MAX_VALUE;
+    var smallInt: Int = Int.MIN_VALUE
+    println("Biggest int: " + bigInt)  //old way
+    println("Smallest int: $smallInt")  //new way
+    
+    //other Data types
+    //Long, Double, Float, Shorty, Byte, Characters
+}
